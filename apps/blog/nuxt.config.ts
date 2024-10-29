3d576c5b7defa23756c07da357ef6484c87c6e4e
ID 0 <id0@posteo.net>
2024-10-29 14:52:52 +0300

    [apps/blog] Enable islands support in Nuxt config.

--- apps/blog/nuxt.config.ts
+++ apps/blog/nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: 'src/',
+  experimental: {
+    componentIslands: {
+      selectiveClient: 'deep',
+    },
+  },
   modules: ['@nuxt/eslint', '@nuxtjs/i18n', 'dayjs-nuxt', '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@vueuse/nuxt'],
   eslint: {
     config: {
