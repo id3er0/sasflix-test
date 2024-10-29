dff98ce4d66fbb82b368764daeee31bbecd26c89
ID 0 <id0@posteo.net>
2024-10-29 03:30:44 +0300

    [apps/blog] Add "dayjs-nuxt".

--- apps/blog/nuxt.config.ts
+++ apps/blog/nuxt.config.ts
@@ -4,11 +4,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/eslint', '@nuxtjs/i18n', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n', '@pinia/nuxt', 'dayjs-nuxt'],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  dayjs: {
+    locales: ['en'],
+    plugins: ['relativeTime', 'utc', 'timezone'],
+    defaultLocale: 'en',
+    defaultTimezone: 'Moscow',
   },
   i18n: {
     locales: ['en'],
